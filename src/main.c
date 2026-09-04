e25366375680177e5973e99fe6d053d871cd721a
Victor Arch-Linux <victorleroux92@gmail.com>
2026-09-04 23:07:59 +0200

    feat(wayland) : initialize display and registry

--- src/main.c
+++ src/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <wayland-client-core.h>
+#include <wayland-client-protocol.h>
+#include <wayland-client.h>
+#include <wayland-util.h>
 int	main(void)
 {
-	return 0;
+	struct wl_display *display;
+	struct wl_registry *registry;
+
+	display = wl_display_connect(NULL);
+	if(display == NULL)
+		return (1);
+	registry = wl_display_get_registry(display);
+
+	wl_registry_destroy(registry);
+	wl_display_disconnect(display);
+	return (0) ;
 }
